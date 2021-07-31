84a211be59261448cc4187f24a1def45f11689c6
Fattakhova <ilnuro4ka01@gmail.com>
2021-07-31 19:33:40 +0500

    started 14th sprint

--- models/user.js
+++ models/user.js
@@ -3,20 +3,28 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
     minlength: 2,
     maxlength: 30,
+    default: 'Жак-Ив Кусто',
   },
   about: {
     type: String,
-    required: true,
     minlength: 2,
     maxlength: 30,
+    default: 'Исследователь',
   },
   avatar: {
     type: String,
+    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+  },
+  email: {
+    type: String,
     required: true,
   },
+  password: {
+    type: String,
+    required: true,
+  }
 });
 
 module.exports = mongoose.model('user', userSchema);
